e51179a4be04431f97e3a07c9eb81082f5968feb
Eldritch Semblance <eldritchsemblance+github@gmail.com>
2018-04-15 20:31:36 -0400

    cors into middleware

--- server.js
+++ server.js
@@ -1,29 +1,9 @@
-const { logger } = require("./utils")
+const { logger } = require('./utils')
+const { cors } = require('./middleware')
+const express = require('express')
 
-const express = require("express")
 const app = express()
-
-// CORS
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
-  next();
-});
-
-
 
 app.use(logger)
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE')
-  if (req.method === 'OPTIONS') {
-    return res.send(204)
-  }
-  next()
-})
+app.use(cors);
