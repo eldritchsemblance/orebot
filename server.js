fac8071e9fb59147945d05d90368522f6129fa09
Eldritch Semblance <eldritchsemblance+github@gmail.com>
2018-04-15 22:08:03 -0400

    bot test

--- server.js
+++ server.js
@@ -40,8 +40,23 @@ bot.on('ready', (evt) => {
 })
 
 bot.on('message', (user, userID, channelID, message, evt) => {
+  if (message.substring(0, 1) == '!') {
+          var args = message.substring(1).split(' ');
+          var cmd = args[0];
 
-  if (message.substring(0, 4) == '/ore') {
+          args = args.splice(1);
+          switch(cmd) {
+              // !ping
+              case 'ping':
+                  bot.sendMessage({
+                      to: channelID,
+                      message: 'Pong!'
+                  });
+              break;
+              // Just add any case commands if you want to..
+           }
+       }
+  /*if (message.substring(0, 1) == '/ore') {
     const args = message.substring(5).split(' ');
     const cmd = args[0];
 
@@ -57,7 +72,7 @@ bot.on('message', (user, userID, channelID, message, evt) => {
     bot.sendMessage({
       to: channelID,
       message: rollText
-    })
+    })*/
   }
 })
 
